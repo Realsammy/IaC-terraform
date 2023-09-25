d98d110ccde7d38d0eef25e1916f5fb996379ec7
Samuel Ekele <samuelekele.bincom@gmail.com>
2023-09-25 22:29:16 +0100

    create a backend storage

--- backend.tf
+++ backend.tf
@@ -1,4 +1,14 @@
+#terraform {
+ # backend "http" {
+ #   }
+#}
+
 terraform {
-  backend "http" {
-    }
+  backend "gcs" {
+    bucket     = "remote_terraform_state2"  # Replace with your desired bucket name
+    prefix     = "tf_state"          # Optionally, specify a prefix for state file
+    credentials = "gcp_key.json"     # Provide the path to your GCP service account key JSON file
+  }
 }
+
+
