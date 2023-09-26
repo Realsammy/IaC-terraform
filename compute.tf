2cef734ddf19ac2da7321e800ce5a2bec46523d7
Samuel Ekele <samuelekele.bincom@gmail.com>
2023-09-26 11:01:57 +0100

    remove my_ prefix

--- compute.tf
+++ compute.tf
@@ -2,13 +2,13 @@ module "gcp_infrastructure" {
   source = "./gcp_vm"
 
   # Input variables
-  vm_name            = var.my_vm_name
-  vm_username        = var.my_vm_username
-  vm_machine_type    = var.my_vm_machine_type
-  environment        = var.my_environment
-  vm_disk_size       = var.my_vm_disk_size
-  gcp_region         = var.my_gcp_region
-  gcp_zone           = var.my_gcp_zone
-  gcp_project_id     = var.my_gcp_project_id
-  gcp_credentials_file = var.my_gcp_credentials_file
+  vm_name               = var.vm_name
+  vm_username           = var.vm_username
+  vm_machine_type       = var.vm_machine_type
+  environment           = var.environment
+  vm_disk_size          = var.vm_disk_size
+  gcp_region            = var.gcp_region
+  gcp_zone              = var.gcp_zone
+  gcp_project_id        = var.gcp_project_id
+  gcp_credentials_file  = var.gcp_credentials_file
 }
