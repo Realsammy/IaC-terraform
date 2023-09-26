a902e12ed5016780bd76c4f61d92866399d0b769
Samuel Ekele <samuelekele.bincom@gmail.com>
2023-09-26 11:35:21 +0100

    commented out var

--- gcp_vm/variable.tf
+++ gcp_vm/variable.tf
@@ -1,42 +1,42 @@
 variable "vm_name" {
   type    = string
-  default = "infrastructure-vm"  
+#  default = "infrastructure-vm"  
 }
 variable "vm_username" {
   type    = string
-  default = "devops" 
+  #default = "devops" 
 }
 
 variable "machine_type" {
   type    = string
-  default = "n1-standard-1" 
+  #default = "n1-standard-1" 
 }
 variable "environment" {
   type    = string
-  default = "compute" 
+  #default = "compute" 
 }
 
 variable "vm_disk_size" {
   type    = number
-  default = 30  
+  #default = 30  
 }
 
 variable "gcp_region" {
   type    = string
-  default = "us-central1"  
+  #default = "us-central1"  
 }
 
 variable "gcp_zone" {
   type    = string
-  default = "us-central1-a"  
+ # default = "us-central1-a"  
 }
 
 variable "gcp_project_id" {
   type    = string
-  default = "proud-curve-399517"  
+ # default = "proud-curve-399517"  
 }
 
 variable "gcp_credentials_file" {
   type    = string
-  default = "./gcp_key.json"  
+  #default = "./gcp_key.json"  
 }
