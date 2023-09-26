7158abed5ba2324b06f1654614b8c4168b72f750
Samuel Ekele <samuelekele.bincom@gmail.com>
2023-09-26 23:09:39 +0100

    rm vm.txt

--- gcp_vm/main.tf
+++ gcp_vm/main.tf
@@ -50,7 +50,8 @@ resource "google_compute_instance" "vm" {
     }
   }
 
-  metadata = {
-    ssh-keys = "${var.vm_username}:${file(".keys/vm_keys.pub")}"
-  }
+  #metadata = {
+   # ssh-keys = "${var.vm_username}:${file(".keys/vm_keys.pub")}"
+
+  #}
 }
