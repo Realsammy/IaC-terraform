736fb687488c8aa0368e893de3312592d3ba166a
Samuel Ekele <samuelekele.bincom@gmail.com>
2023-09-26 11:25:56 +0100

    comment out variables

--- backend.tf
+++ backend.tf
@@ -5,9 +5,9 @@
 
 terraform {
   backend "gcs" {
-    bucket     = "remote_terraform_state2"  # Replace with your desired bucket name
-    prefix     = "tf_state"          # Optionally, specify a prefix for state file
-    credentials = "gcp_key.json"     # Provide the path to your GCP service account key JSON file
+    bucket     = "remote_terraform_state2"  
+    prefix     = "tf_state"          
+    credentials = "gcp_key.json"     
   }
 }
 
