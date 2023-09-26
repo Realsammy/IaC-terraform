a77f4ff8f11d994d5f70f3b5b4c7e7e36dd1404e
Samuel Ekele <samuelekele.bincom@gmail.com>
2023-09-26 19:23:13 +0100

    change project id

--- compute.tf
+++ compute.tf
@@ -6,7 +6,7 @@ module "gcp_infrastructure" {
   machine_type  = "n1-standard-1"
   vm_disk_size   = 30
   gcp_region     = "us-central1"
-  gcp_zone       = "us-central1-a"
+  gcp_zone       = "us-central1-b"
   gcp_project_id = "terrafor4gcp"
   gcp_credentials_file = file("./gcp_key.json")
 }
