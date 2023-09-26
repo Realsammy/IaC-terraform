1fb5dff7bcf169d5cd89de835b938cd16c2c714d
Samuel Ekele <samuelekele.bincom@gmail.com>
2023-09-26 19:14:28 +0100

    change project id

--- compute.tf
+++ compute.tf
@@ -1,6 +1,6 @@
 module "gcp_infrastructure" {
   source         = "./gcp_vm"
-  vm_name        = "vm_name"
+  vm_name        = "my-instance"
   vm_username    = "devops"
   environment    = "environment"
   machine_type  = "n1-standard-1"
