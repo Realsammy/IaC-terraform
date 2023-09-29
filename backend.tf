6fb574c5511f809da8765e67c1d4eec08b499397
Samuel Ekele <samuelekele.bincom@gmail.com>
2023-09-29 11:26:50 +0100

    use apine image

--- backend.tf
+++ backend.tf
@@ -1,14 +1,14 @@
-terraform {
-  backend "http" {
-    }
-}
-
 #terraform {
- # backend "gcs" {
-   # bucket     = "remote_terraform_state2"  
-   # prefix     = "tf_state"          
-  #  credentials = "gcp_key.json"     
- # }
+#  backend "http" {
+  #  }
 #}
+
+terraform {
+  backend "gcs" {
+    bucket     = "remote_terraform_state2"  
+    prefix     = "tf_state"          
+    credentials = "gcp_key.json"     
+  }
+}
 
 
