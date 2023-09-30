6f49f975301ac50709ae6ddc1dcd69ea9b0239bb
Samuel Ekele <samuelekele.bincom@gmail.com>
2023-09-30 16:48:46 +0100

    correct the backen

--- backend.tf
+++ backend.tf
@@ -1,14 +1,18 @@
+#terraform {
+ # backend "http" {
+  #  }
+#}
+
 terraform {
-  backend "http" {
+  backend "gcs" {
     }
 }
-
 #terraform {
- # backend "gcs" {
-  #  bucket     = "remote_terraform_state2"  
+  #backend "gcs" {
+   # bucket     = "remote_terraform_state2"  
    # prefix     = "tf_state"          
-    #credentials = "gcp_key.json"     
-  #}
+   # credentials = "gcp_key.json"     
+ # }
 #}
 
 
