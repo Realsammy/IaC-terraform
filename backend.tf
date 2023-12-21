2345fb616e58eae643d1dc11ff51e5561387ecd3
Samuel Ekele <realsammy86@gmail.com>
2023-12-21 18:27:45 +0100

    first commit

--- backend.tf
+++ backend.tf
@@ -1,18 +1,15 @@
-terraform {
-  backend "http" {
-    }
-}
+#terraform {
+ # backend "http" {
+  #  }
+#}
 
-#terraform {
-#  backend "gcs" {
- #   }
-#}
-#terraform {
-  #backend "gcs" {
-   # bucket     = "remote_terraform_state2"  
-   # prefix     = "tf_state"          
-   # credentials = "gcp_key.json"     
- # }
-#}
+
+terraform {
+  backend "gcs" {
+    bucket     = "remote_terraform_state2"  
+    prefix     = "tf_state"          
+    credentials = "gcp_key.json"     
+  }
+}
 
 
